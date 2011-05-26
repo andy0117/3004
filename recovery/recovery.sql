8b8e67937cdc91f8e02527f5b65627606f6bcd0f
Louis Stowasser <louisstow@gmail.com>
2011-05-26 22:40:37 +1000

    * New recovery scripts

--- recovery/recovery.sql
+++ recovery/recovery.sql
@@ -9,32 +9,43 @@ CREATE TABLE Questions(
 	question VARCHAR2(255) NOT NULL
 );
 
-CREATE TABLE Comparitives(
-	questID NUMBER(6) PRIMARY KEY,
-	compareTo NUMBER(6)
-);
-
 CREATE TABLE Responses(
 	reponseID NUMBER(6) PRIMARY KEY,
 	keypad CHAR(1),
 	response VARCHAR2(255) NOT NULL,
-	weight NUMBER(6), 		-- Bad practice but meh
-	compareTo NUMBER(6), 	-- Let's just call it Denormalization for performance tuning
 	questID NUMBER(6) NOT NULL,
 	correct CHAR(1) NOT NULL
 );
 
+CREATE TABLE Comparitives(
+	questID NUMBER(6) PRIMARY KEY,
+	compareTo NUMBER(6) NOT NULL
+);
+
+CREATE TABLE Ranking(
+	reponseID NUMBER(6) PRIMARY KEY,
+	weight NUMBER(6)
+);
+
 CREATE TABLE Answers(
 	answerID NUMBER(6) PRIMARY KEY,
-	response VARCHAR2(255), -- One of these is going to be NULL
-	keypad CHAR(1),	        -- Means a lot less tables though :)
 	questID NUMBER(6) NOT NULL
+);
+
+CREATE TABLE KeyAnswers(
+	answerID NUMBER(6) PRIMARY KEY,
+	keypad CHAR(1) NOT NULL
+);
+
+CREATE TABLE ShortAnswers(
+	answerID NUMBER(6) PRIMARY KEY,
+	response VARCHAR2(255) NOT NULL
 );
 
 CREATE TABLE Polls(
 	pollID NUMBER(6) PRIMARY KEY,
 	pollName VARCHAR2(255) NOT NULL,
-	location VARCHAR2(255) NOT NULL,
+	location VARCHAR2(255) NOT NULL
 );
 
 CREATE TABLE Users(
@@ -46,8 +57,9 @@ CREATE TABLE Users(
 );
 
 CREATE TABLE Assigned(
-	userID NUMBER(6) PRIMARY KEY,
-	pollID NUMBER(6) PRIMARY KEY,
+	userID NUMBER(6),
+	pollID NUMBER(6),
 	role VARCHAR(255) NOT NULL,
-	CONSTRAINT userRole CHECK (role IN ('Web User', 'Key User', 'Poll Master', 'Poll Creator', 'Poll Admin', 'System Admin'))
+	CONSTRAINT userRole CHECK (role IN ('Web User', 'Key User', 'Poll Master', 'Poll Creator', 'Poll Admin', 'System Admin')),
+	PRIMARY KEY (userID, pollID)
 );
