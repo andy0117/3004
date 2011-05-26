1228e520561a6eaca03828026a035ac3c33dee38
Louis Stowasser <louisstow@gmail.com>
2011-05-26 21:44:30 +1000

    * added other stuff back

--- prototype/assets/scripts.js
+++ prototype/assets/scripts.js
@@ -15,35 +15,42 @@ $(function() {
 		$(this).parent().parent().find(".ranking").toggle();
 	});
 	
-	var question = $("div.question").clone();
+	var question = $("div.question").clone(),
+		questionCount = 1;
 	
 	/**
 	* Add a new question
 	*/
 	$("#newq").click(function() {
 		question.clone().appendTo(document.body);
+		questionCount++;
 	});
 	
 	/**
 	* Add a new response
 	*/
 	$("button.newr").live("click", function() {
-		var parent = $(this).parent(),
-			response = $(this).next();
+		var parent = $(this).next(),
+			response = $(this).next().children(":first-child");
 		response.clone().appendTo(parent);
 	});
 	
 	/**
-	* Cancel a question
+	* Cancel a question. Not allowed to remove all questions.
 	*/
 	$("button.remq").live("click", function() {
-		$(this).parent().remove();
+		if(questionCount > 1) {
+			$(this).parent().remove();
+			questionCount--;
+		}
 	});
 	
 	/**
 	* Cancel a response
 	*/
 	$("button.remr").live("click", function() {
-		$(this).parent().remove();
+		if($(this).parent().parent().children("div").size() > 1) {
+			$(this).parent().remove();
+		}
 	});
 });
