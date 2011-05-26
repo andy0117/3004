35d5727d8775d819f1656c07b645cf60a9b44413
Darren Goodair <darren.goodair@gmail.com>
2011-05-26 21:43:55 +1000

    Added a drop table script, makes it easier with the current issues in the recovery script

--- recovery.sql
+++ recovery.sql
@@ -1,7 +1,7 @@
 CREATE TABLE Questions(
 questID NUMBER(6) PRIMARY KEY,
-demographic CHAR(1),
-responseType CHAR(1),
+demographic CHAR(1) NOT NULL,
+responseType CHAR(1) NOT NULL,
 question VARCHAR2(255) NOT NULL
 );
 
@@ -10,23 +10,20 @@ questID NUMBER(6) PRIMARY KEY,
 compareTo NUMBER(6)
 );
 
-CREATE TABLE Rankings(
-questID NUMBER(6) PRIMARY KEY,
-responseID NUMBER(6) PRIMARY KEY,
-weight NUMBER(6)
-);
-
 CREATE TABLE Responses(
 reponseID NUMBER(6) PRIMARY KEY,
 keypad CHAR(1),
 response VARCHAR2(255) NOT NULL,
-questID NUMBER(6) NOT NULL
+weight NUMBER(6), -- Bad practice but meh
+compareTo NUMBER(6), -- Let's just call it Denormalization for performance tuning
+questID NUMBER(6) NOT NULL,
+correct CHAR(1) NOT NULL
 );
 
 CREATE TABLE Answers(
 answerID NUMBER(6) PRIMARY KEY,
-response VARCHAR2(255),
-keypad CHAR(1),
+response VARCHAR2(255), -- One of these is going to be NULL
+keypad CHAR(1), -- Means a lot less tables though :)
 questID NUMBER(6) NOT NULL
 );
 
@@ -47,6 +44,6 @@ location VARCHAR2(255) NOT NULL
 CREATE TABLE Assigned(
 userID NUMBER(6) PRIMARY KEY,
 pollID NUMBER(6) PRIMARY KEY,
-role VARCHAR(255),
+role VARCHAR(255) NOT NULL,
 CONSTRAINT userRole CHECK (role IN ('Web User', 'Key User', 'Poll Master', 'Poll Creator', 'Poll Admin', 'System Admin'))
 );
